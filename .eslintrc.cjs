d34a82767266c626648849b7bb809e45b31c11c1
ranqi <319887843@qq.com>
2023-02-16 16:19:03 +0800

    feat：完成prettier配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,26 @@
-// .eslintrc.js
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        // 'plugin:vue/essential', // vue2
-        'plugin:vue/vue3-recommended', // vue3
-        'standard'
-    ],
-    parserOptions: {
-        ecmaVersion: 13,
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module'
-    },
-    plugins: [
-        'vue',
-        '@typescript-eslint'
-    ],
-    rules: {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'standard',
+    'plugin:prettier/recommended' // 新增
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+    'prettier' // 新增
+  ],
+  rules: {
+    'prettier/prettier': 'error', // 新增
+    'vue/comment-directive': 'off'
+  }
 }
